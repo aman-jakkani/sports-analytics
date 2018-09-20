f81f04a4f3fefa3f801e075aaea11e4c87fd82b9
Darwin Schlenk <darwin.schlenk@stud.hs-hannover.de>
2018-09-20 18:06:57 +0200

    Implemented simple logging

--- backend/src/main/java/edu/sportanalytics/control/Main.java
+++ backend/src/main/java/edu/sportanalytics/control/Main.java
@@ -6,9 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.logging.Logger;
 
-public class Main
+class Main
 {
+    public final static Logger log = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args)
     {
         JFrame serverFrame = new JFrame("Sportanalytics Server");
@@ -27,6 +30,7 @@ public class Main
         serverFrame.setContentPane(buildMainFrame());
 
         serverFrame.setVisible(true);
+        log.info("Main window initialized");
     }
 
     private static Container buildMainFrame()
@@ -56,6 +60,7 @@ public class Main
 
     private static void onExit(int status)
     {
+        log.info("Exiting with status: " + status);
         System.exit(status);
     }
 }

b7750c6dc6e1721ff04af11298943fffcf9a6927
Oliver Nimz <oliver.nimz@hs-hannover.de>
2018-10-26 19:49:08 +0200

    Moved League-query & minor changes

--- src/main/java/edu/sportanalytics/database/DBAccess.java
+++ src/main/java/edu/sportanalytics/database/DBAccess.java
@@ -2,62 +2,31 @@ package edu.sportanalytics.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
 public class DBAccess {
 	private static DBAccess instance;
 	private static Connection conn;
-	private static ResultSet rs;
+	private static final Logger log = Logger.getLogger(DBAccess.class.getName());
 
 	private DBAccess() {
 		try {
+			log.info("Creating DB-Connection");
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "sports-analytics");
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+			log.severe(e.getMessage());
 		}
-	}
-
-	public ArrayList<Soccer_League> createQueryLeagues(String query) {
-		Statement stmt = null;
-		ArrayList<Soccer_League> list = new ArrayList<Soccer_League>();
-		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(query);
-
-			/* Placeholder for rs-processing */
-			while (rs.next()) {
-				list.add(new Soccer_League(rs.getString("NAME"), rs.getInt("LEAGUE_ID")));
-			}
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		} finally {
-			try {
-				stmt.close();
-				rs.close();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-			}
-
-		}
-		return list;
-
 	}
 
 	/* Close DB-Connection and reset classobject instance */
 	public void closeConnection() {
 		try {
+			log.info("Closing DB-Connection");
 			conn.close();
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+			log.severe(e.getMessage());
 		}
 		instance = null;
 	}
--- src/main/java/edu/sportanalytics/database/SoccerController.java
+++ src/main/java/edu/sportanalytics/database/SoccerController.java
@@ -1,17 +1,22 @@
 package edu.sportanalytics.database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class SoccerController extends DatabaseController
 
 {
 	private List<Soccer_League> leaguesList;
+	private static final Logger log = Logger.getLogger(SoccerController.class.getName());
 
 	public SoccerController() {
 		super();
 
-		leaguesList = super.getDb().createQueryLeagues("SELECT NAME, LEAGUE_ID FROM SOCCER02.LEAGUE");
+		leaguesList = createQueryLeagues("SELECT NAME, LEAGUE_ID FROM SOCCER02.LEAGUE");
 
 	}
 
@@ -33,5 +38,33 @@ public class SoccerController extends DatabaseController
 
 	public List<String> getGame(String season, String team) {
 		return null;
+	}
+
+	public ArrayList<Soccer_League> createQueryLeagues(String query) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Soccer_League> list = new ArrayList<Soccer_League>();
+		try {
+			stmt = DBAccess.getConn().createStatement();
+			rs = stmt.executeQuery(query);
+
+			/* Placeholder for rs-processing */
+			while (rs.next()) {
+				list.add(new Soccer_League(rs.getString("NAME"), rs.getInt("LEAGUE_ID")));
+			}
+
+		} catch (SQLException e) {
+			log.severe(e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+				rs.close();
+			} catch (SQLException e) {
+				log.severe(e.getMessage());
+			}
+
+		}
+		return list;
+
 	}
 }

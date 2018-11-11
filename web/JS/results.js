fde6e285481beda49b29f8faee93f2588c70c474
jmurphy8795 <jmurphy8795@tamu.edu>
2018-11-10 21:21:32 -0600

    updates

---
+++ web/JS/results.js
@@ -0,0 +1,20 @@
+ function execute() {
+		   document.getElementById('hiddenText').style.display="block";
+		   		showSecondStatisticInput();
+		   		showAnalysisInput();
+		   		showFirstStatisticInput();
+	}
+
+function showFirstStatisticInput() {
+        document.getElementById('stat1').innerHTML = 
+                    document.getElementById("firstStatistic").value;
+    }
+function showSecondStatisticInput() {
+        document.getElementById('stat2').innerHTML = 
+                    document.getElementById("secondStatistic").value;
+    }
+
+function showAnalysisInput() {
+        document.getElementById('analyze').innerHTML = 
+                    document.getElementById("analysis").value;
+    }

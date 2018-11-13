46b24df522b256f2518de161ee43f67a1b8189e9
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2018-11-13 10:58:01 -0600

    added get resource functinos in dropdown.js

--- web/JS/dropdown.js
+++ web/JS/dropdown.js
@@ -1,42 +1,126 @@
 $(document).ready(function() {
-  $("#sport, #league, #team, #season").change(function() {
+    $("#sport, #league, #team, #season").change(function() {
 
-    if ($(this).attr('id') == 'sport') {
-      var leagueName = $(this).val();
-      $("#league").html(leaguesList[leagueName]);
+      if ($(this).attr('id') == 'sport') {
+        var sport = $(this).val();
+        $("#league").html(getLeagues(sport));
 
-      var stat1 = $(this).val();
-      $("#firstStatistic").html(statsList[stat1]);
+        statsRequested(sport)
+        $("#firstStatistic").html(getStats(sport));
 
-      var stat2 = $(this).val();
-      $("#secondStatistic").html(statsList[stat2]);
-    } 
+        statsRequested(sport)
+        $("#secondStatistic").html(getStats(sport));
+      } 
 
-    else if ($(this).attr('id') == 'league') {
-      var teamName = $("#league").val();
-      $("#team").html(teamsList[teamName]);
+      else if ($(this).attr('id') == 'league') {
+        var league = $("#league").val();
+        $("#team").html(getTeams(sport, league));
+      }
+
+      else if ($(this).attr('id') == 'team') {
+        var team = $("#team").val();
+        $("#season").html(getSeasons(sport, league, team));
+      }
+
+      else if ($(this).attr('id') == 'season') {
+        var season = $("#season").val();
+        $("#game").html(getGames(sport, league, team, season));
+      }
     }
+  );
+});
 
-	  else if ($(this).attr('id') == 'team') {
-      var seasonName = $("#team").val();
-      $("#season").html(seasonsList[seasonName]);
-    }
+// Note: need to include loadResources() ?
 
-    else if ($(this).attr('id') == 'season') {
-      var gameName = $("#season").val();
-      $("#game").html(gamesList[gameName]);
-    }
+// get string of leagues
+function getLeagues(sport) {
 
+  var parameters = [["sports", sport], ];
+  var json = getRestResource("LeagueListResource", parameters);
+
+  var htmlLeagueString = "<option value=0 >--Make a choice--</option>";
+
+  // return default option if there are no leagues in the sport, or if no sport is selected
+  if (json.leagues.length == 0) return htmlLeagueString;
+
+  // create string for every league
+  for (i = 0; i < json.leagues.length; ++i){
+    htmlLeagueString = htmlLeagueString.concat("<option value=" + (i + 1) + ">" + json.leagues[i] + "</option>");
   }
-);
+
+  return htmlLeagueString; 
+};
 
 
+// return string of possible stats for a given sport
+function getStats(sport){
+  // don't have stats resource available yet
+  return "";
+}
+
+// return string of all teams for a given sport/league
+function getTeams(sport, league){
+  var parameters = [["sports", sport], ["league", league]];
+  var json = getRestResource("TeamListResource", parameters);
+
+  var htmlTeamString = "<option value=0 >--Make a choice--</option>";
+
+  // return default string if there are no teams for a specific league, or if no league is selected
+  if (json.teams.length == 0) return htmlTeamString;
+
+  for (i = 0; i < json.teams.length; ++i){
+    htmlTeamString = htmlTeamString.concat("<option value=" + (i + 1) + ">" + json.teams[i] + "</option>")
+  }
+
+  return htmlTeamString;
+}
+
+
+// return string of all seasons for a given sport/league/team
+function getSeasons(sport, league, team){
+  var parameters = [["sports", sport], ["league", league], ["team", team]];
+  var json = getRestResource("SeasonListResource", parameters);
+
+  var htmlSeasonString = "<option value=0 >--Make a choice--</option>";
+
+  // return default string if there are no teams for a specific league, or if no league is selected
+  if (json.seasons.length == 0) return htmlSeasonString;
+
+  for (i = 0; i < json.seasons.length; ++i){
+    htmlSeasonString = htmlSeasonString.concat("<option value=" + (i + 1) + ">" + json.seasons[i] + "</option>")
+  }
+
+  return htmlSeasonString;
+}
+
+
+/* This REST resource isn't used in the example HTML file, need to confirm it is correct */
+// return string of all games for a given sport/league/team/season
+function getGames(sport, league, team, season){
+  var parameters = [["sports", sport], ["league", league], ["team", team], ["season", season]];
+  var json = getRestResource("MatchListResource", parameters);
+
+  var htmlMatchString = "<option value=0 >--Make a choice--</option>";
+
+  // return default string if there are no teams for a specific league, or if no league is selected
+  if (json.matches.length == 0) return htmlMatchString;
+
+  for (i = 0; i < json.matches.length; ++i){
+    htmlMatchString = htmlMatchString.concat("<option value=" + (i + 1) + ">" + json.matches[i] + "</option>")
+  }
+
+  return htmlMatchString;
+}
+
+
+
+/*
  // once database is created, we won't need any of these lists. we'll just load the data dynamically
 
   var leaguesList = [
     "<option value='0'>--Make a choice--</option>",
-    "<option value='0'>--Make a choice--</option><option value='1'>NBA</option>",
-    "<option value='0'>--Make a choice--</option><option value='2'>La Liga</option><option value='3'>Premier League</option>"
+    "<option value='0'>--Make a choice--</option> <option value='1'>NBA</option>",
+    "<option value='0'>--Make a choice--</option> <option value='2'>La Liga</option> <option value='3'>Premier League</option>"
   ];
 
 
@@ -85,7 +169,6 @@ $(document).ready(function() {
     "<option value='0'>--Make a choice--</option><option value='1'>Rebounds</option><option value='2'>Shooting Percentage</option>",
     "<option value='0'>--Make a choice--</option><option value='3'>Goals</option><option value='4'>Assists</option>"
   ];
-  
+
 });
-
-
+*/

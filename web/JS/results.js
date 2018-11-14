79d346043577065d40bca89397aa8ac3365e9b44
jmurphy8795 <jmurphy8795@tamu.edu>
2018-11-14 00:02:51 -0600

    implemented a simple chart

--- web/JS/results.js
+++ web/JS/results.js
@@ -9,4 +9,26 @@ function showData() {
         document.getElementById('chosenFirstStat').innerHTML = document.getElementById("firstStatistic").value;
         document.getElementById('chosenSecondStat').innerHTML = document.getElementById("secondStatistic").value;
         document.getElementById('chosenAnalysis').innerHTML = document.getElementById("analysis").value;   
+
+let myChart = document.getElementById('myChart').getContext('2d');
+
+let massPopChart = new Chart(myChart, {
+	type: 'bar', //bar, horizontal bar, pie, line, doughnut, radar, polarArea
+	data:{
+	labels: ['Rockets','Raptors','Warriors','Celtics','76ers','Cavaliers'],
+	datasets: [{
+		label:'Wins',
+		data: [
+			65,
+			59,
+			58,
+			55,
+			52,
+			50
+		]
+	}]
+	},
+	options:{}
+});
+
 }

dbe5505114ddc53e2bc5c7fc47474b60b894be87
Oliver Nimz <oliver.nimz@stud.hs-hannover.de>
2018-11-21 00:28:26 +0100

    Minor change

--- src/main/java/edu/sportanalytics/guiinterface/Token.java
+++ src/main/java/edu/sportanalytics/guiinterface/Token.java
@@ -4,55 +4,63 @@ import edu.sportanalytics.database.SportsEnum;
 
 import java.util.HashMap;
 
-public class Token
-{
-    private SportsEnum type;
-    private String league;
-    private String team;
-    private String season;
-    private String match;
+public class Token {
+	private SportsEnum type;
+	private String league;
+	private String team;
+	private String season;
+	private String match;
 
-    public SportsEnum getSports() {
-        return type;
-    }
+	public SportsEnum getSports() {
+		return type;
+	}
 
-    public String getLeague() {
-        return league;
-    }
+	public String getLeague() {
+		return league;
+	}
 
-    public String getTeam() {
-        return team;
-    }
+	public String getTeam() {
+		return team;
+	}
 
-    public String getSeason() {
-        return season;
-    }
+	public String getSeason() {
+		return season;
+	}
 
-    public String getMatch() {
-        return match;
-    }
+	public String getMatch() {
+		return match;
+	}
 
-    public Token(SportsEnum type, String league, String team, String season, String match)
-    {
-        this.type = type;
-        this.league = league;
-        this.team = team;
-        this.season = season;
-        this.match = match;
+	public int getMatchID() {
+		int j = 0;
+		String matchTemp = getMatch();
+		for (int i = 0; i < matchTemp.length(); i++) {
+			if (matchTemp.charAt(i) == ':') {
+				j = i;
+			}
+		}
+		matchTemp = match.substring(j + 1, matchTemp.length() - 1);
+		return Integer.parseInt(matchTemp);
+	}
 
+	public Token(SportsEnum type, String league, String team, String season, String match) {
+		this.type = type;
+		this.league = league;
+		this.team = team;
+		this.season = season;
+		this.match = match;
 
-        tokenMap.put(this.hashCode(), this);
-    }
+		tokenMap.put(this.hashCode(), this);
+	}
 
-    @Override public int hashCode()
-    {
-        return java.util.Objects.hash(type, league, team, season, match);
-    }
+	@Override
+	public int hashCode() {
+		return java.util.Objects.hash(type, league, team, season, match);
+	}
 
-    private static HashMap<Integer, Token> tokenMap = new HashMap<>();
+	private static HashMap<Integer, Token> tokenMap = new HashMap<Integer, Token>();
 
-    public static Token getToken(int key)
-    {
-        return tokenMap.get(key);
-    }
+	public static Token getToken(int key) {
+		return tokenMap.get(key);
+	}
 }

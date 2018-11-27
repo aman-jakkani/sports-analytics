a793d0cf0190728922f20561d09ba4f7c50d742d
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2018-11-27 03:55:24 -0600

    get stat resources

--- web/JS/results.js
+++ web/JS/results.js
@@ -1,4 +1,3 @@
-// can't generate another token
 function showData() {
         document.getElementById('hiddenText').style.display="block";
 
@@ -19,7 +18,7 @@ function showData() {
         var parameters = [["sports", "Soccer"],];
 
         league = getRestResource("LeagueListResource", parameters);
-        parameters.push(["league", league.leagues[0]]);
+        parameters.push(["league", league.leagues[1]]);
 
         team = getRestResource("TeamListResource", parameters);
         parameters.push(["team", team.teams[0]]);
@@ -35,55 +34,69 @@ function showData() {
         var token = getRestResource("TokenResource", parameters);
         console.log("Token: " + token["token"]);
 
+        var teams = getRestResource("HomeAndAwayTeamListResource", token["token"]);
+        var ballPossession = getRestResource("BallPossessionResource", token["token"]);
+        var yellowCards = getRestResource("YellowCardStatResource", token["token"]);
+        var cornerStats = getRestResoruce("CornerStatsResource", token["token"]);
+        var redCards = getRestResource("RedCardsStatResource", token["token"]);
+        var foulStats = getRestResource("FoulsStatResource", token["token"]);
+        
         // ------------------------------------------------------------------
 
         // don't plot anything if a chart type isn't selected
-        if (document.getElementById("chartType").value == "null") return;
+        if (document.getElementById("chartType").value == "null") {
+                console.log("No chart selected. Chart plotting skipped");
+                return;
+        }
 
+        var config = {
+                type: document.getElementById("chartType").value, // bar, horizontal bar, pie, line, doughnut, radar, polarArea
+                data: {
+                        labels: ["Rockets", "Mavericks", "Lakers", "Celtics", "Kings", "Thunder"],
+                        datasets: [{
+                        label: '# of Technical Fouls',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                        }]
+                },
+                options: {
+                        scales: {
+                                xAxes: [{
+                                ticks: {
+                                beginAtZero:true
+                                }
+                        }],
+                        yAxes: [{
+                                ticks: {
+                                beginAtZero:true
+                                }
+                        }]
+                        }
+                }
+        }
+
+
+        // Create chart
         var ctx = document.getElementById("myChart");
 
         // prevent chart from showing old data when mouse is scrolled over it
         if(window.bar != undefined) window.bar.destroy();
 
-        window.bar = new Chart(ctx, {
-        type: document.getElementById("chartType").value, // bar, horizontal bar, pie, line, doughnut, radar, polarArea
-        data: {
-                labels: ["Rockets", "Mavericks", "Lakers", "Celtics", "Kings", "Thunder"],
-                datasets: [{
-                label: '# of Technical Fouls',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-                }]
-        },
-        options: {
-                scales: {
-                        xAxes: [{
-                        ticks: {
-                        beginAtZero:true
-                        }
-                }],
-                yAxes: [{
-                        ticks: {
-                        beginAtZero:true
-                        }
-                }]
-                }
-        }
-    });   
+        window.bar = new Chart(ctx, config);   
 }

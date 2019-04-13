5c2760267031d268c37864ff6da98395e1f328c7
Oliver Nimz <oliver.nimz@stud.hs-hannover.de>
2019-04-13 16:54:28 +0200

    added dynamic sidebar

--- web/js/utils.js
+++ web/js/utils.js
@@ -153,6 +153,20 @@ function createPlayerTable(data){
 }
 
 
+function w3_open() {
+    document.getElementById("main").style.marginLeft = "15%";
+    document.getElementById("mySidebar").style.width = "15%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+
+function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
+        
+
 
 /*
 function generateChart(chartType, homeTeamName, awayTeamName, homeTeamDataParam, awayTeamDataParam, label){

0335a8eb0f79c889c0ba3c75660b4ffcb461ec5d
Aman <aj280598@gmail.com>
2019-04-14 12:15:23 -0500

    finishing test cases

--- src/main/java/edu/sportanalytics/test/BasketballControllerTest.java
+++ src/main/java/edu/sportanalytics/test/BasketballControllerTest.java
@@ -30,11 +30,24 @@ class BasketballControllerTest {
     }
 
     @Test
+    public void testDbConnection() {
+        //Default Params
+        String server = DBAccess.getInstance().getServerURL();
+        String port = DBAccess.getInstance().getPort();
+        String sid = DBAccess.getInstance().getSid();
+        String username = DBAccess.getInstance().getUsername();
+        String pw = ""; //Enter db password here before testing but DO NOT push pw to git!!
+        char[] pwArr = pw.toCharArray();
+
+        DBAccess.getInstance().setDBParams(server, port, sid, username, pwArr);
+    }
+
+    @Test
     void getLeagues() {
         List<String> league = this.bc.getLeagues();
         List<String> toComp = new ArrayList<>();
         toComp.add("NBA");
-        Assert.assertEquals(league, toComp);
+        Assert.assertEquals(toComp, league);
     }
 
     @Test
@@ -44,55 +57,71 @@ class BasketballControllerTest {
         listOfTeams.add("Hawks");
         listOfTeams.add("Celtics");
         listOfTeams.add("Cavaliers");
-        Assert.assertEquals(teams.subList(0,3), listOfTeams);
+        Assert.assertEquals(listOfTeams,teams.subList(0, 3));
     }
 
+    @Test
+    void getSeason() {
+        List<String> season = bc.getSeason("NBA", "Celtics");
+        List<String> listOfSeasons = new ArrayList<>();
+        listOfSeasons.add("2009");
+        listOfSeasons.add("2011");
+        listOfSeasons.add("2014");
+        Assert.assertEquals(listOfSeasons,season.subList(0, 3));
+    }
 
     @Test
     void getGame() {
+        //List<String> games = bc.getGame("2018", "Celtics");
+        //System.out.println(games);
     }
 
     @Test
     void getHomeAndAwayTeam() {
+        //testing Celtics vs Lakers, 2017 season
+        List<String> teams = bc.getHomeAndAwayTeam("21700161");
+        List<String> listOfTeams = new ArrayList<>();
+        listOfTeams.add("Celtics");
+        listOfTeams.add("Lakers");
+        Assert.assertEquals(listOfTeams, teams);
     }
 
     @Test
     void homeScoreById() {
+        //testing 76ers vs Knicks, 2017 season
+        String score = bc.HomeScoreById("21700842");
+        Assert.assertEquals(score, "108");
     }
 
     @Test
     void guestScoreById() {
-    }
-
-    @Test
-    void getAttendanceByGame() {
-    }
-
-    @Test
-    void getLeaguesList() {
-    }
-
-    @Test
-    void getTeamList() {
+        //testing Heat vs Spurs, 2017 season
+        String score = bc.GuestScoreById("21700059");
+        Assert.assertEquals(score, "117");
     }
 
     @Test
     void getHomePlayerList() {
+        //testing Bulls vs Mavericks, 2018 season
+        List<String> homeplayers = bc.getHomePlayerList("21800193");
+        List<String> listOfPlayers = new ArrayList<>();
+        listOfPlayers.add("Harrison Barnes");
+        listOfPlayers.add("Luka Doncic");
+        listOfPlayers.add("DeAndre Jordan");
+        Assert.assertEquals(listOfPlayers,homeplayers.subList(0,3));
     }
 
     @Test
-    public void testDbConnection()
-    {
-        //Default Params
-        String server = DBAccess.getInstance().getServerURL();
-        String port = DBAccess.getInstance().getPort();
-        String sid = DBAccess.getInstance().getSid();
-        String username = DBAccess.getInstance().getUsername();
-        String pw = ""; //Enter db password here before testing but DO NOT push pw to git!!
-        char[] pwArr = pw.toCharArray();
-
-        DBAccess.getInstance().setDBParams(server, port, sid, username, pwArr );
+    void getAwayPlayerList() {
+        //testing Celtics vs Lakers, 2017 season
+        List<String> awayplayers = bc.getAwayPlayerList("21700161");
+        List<String> listOfPlayers = new ArrayList<>();
+        listOfPlayers.add("Jayson Tatum");
+        listOfPlayers.add("Marcus Morris");
+        listOfPlayers.add("Aron Baynes");
+        Assert.assertEquals(listOfPlayers,awayplayers.subList(0,3));
     }
+
 
     @Test
     void getPlayer() {
@@ -136,53 +165,5 @@ class BasketballControllerTest {
 
     @Test
     void getDb() {
-    }
-
-    @Test
-    void getLeagues1() {
-    }
-
-    @Test
-    void getTeams1() {
-    }
-
-    @Test
-    void getSeason1() {
-    }
-
-    @Test
-    void getGame1() {
-    }
-
-    @Test
-    void getHomeAndAwayTeam1() {
-    }
-
-    @Test
-    void getAwayTeamPlayerID1() {
-    }
-
-    @Test
-    void getHomeTeamPlayerID1() {
-    }
-
-    @Test
-    void getAwayPlayerList1() {
-    }
-
-    @Test
-    void getHomePlayerList1() {
-    }
-
-    @Test
-    void getPlayer1() {
-    }
-
-    @Test
-    void getCube1() {
-    }
-
-    @Test
-    void getRollup1() {
     }
 }
